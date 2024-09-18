c386734acb87a41eac002d434e3148c4ef264112
Tasmeer Naeem <tasmeernaeem890@gmail.com>
2024-09-18 19:28:35 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="flex text-center text-blue-700  text-3xl font-bold underline">
+        hello
+      </div>
+    </>
+  )
+}
+
+export default App
